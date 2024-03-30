1eea270a0c4b2661d2da9925d9ff952df6cb4e91
haru-0205 <rm23081b@osaka-pct.ac.jp>
2024-03-30 22:23:04 +0900

    configure lilypond-book

--- flake.nix
+++ flake.nix
@@ -4,38 +4,43 @@
   inputs = {
     nixpkgs = { url = "github:NixOS/nixpkgs/nixpkgs-unstable"; };
     flake-utils = { url = "github:numtide/flake-utils"; };
-    home-manager = {
-     url = "github:nix-community/home-manager";
-     inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, home-manager }:
-    flake-utils.lib.eachDefaultSystem ( system:
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
       let
         inherit (nixpkgs.lib) optional;
-	pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs { inherit system; };
+        tex = (pkgs.texlive.combine {
+          inherit (pkgs.texlive) scheme-full;
+          });
       in
-      {
-        homeConfigurations = {
-	   "nixos@nixos" = home-manager.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages."x86_64-linux"; # WSL
-          modules = [ ./home/WSL.nix ];
-          };
-	};
+       {
         devShell = pkgs.mkShell {
-	  buildInputs = [
-	    pkgs.neovim
-	    pkgs.mpv
-	    pkgs.home-manager
-	    pkgs.timidity
-	    pkgs.soundfont-fluid
-	    pkgs.fluidsynth
-	    pkgs.ffmpeg
-	    pkgs.git
-      pkgs.deno
-      pkgs.lilypond
-	  ];
-	};
-      });
+        buildInputs = [
+        pkgs.neovim
+        pkgs.mpv
+        pkgs.home-manager
+        pkgs.timidity
+        pkgs.soundfont-fluid
+        pkgs.fluidsynth
+        pkgs.ffmpeg
+        pkgs.git
+        pkgs.deno
+        pkgs.lilypond
+        pkgs.alsa-oss
+        pkgs.alsa-lib
+        pkgs.ripgrep
+        pkgs.fd
+        pkgs.okular
+        pkgs.noto-fonts
+        pkgs.noto-fonts-extra
+        pkgs.noto-fonts-cjk
+        pkgs.noto-fonts-emoji
+        tex
+        ];
+      };
+    });
 }
+
+
--- sample.ly
+++ sample.ly
@@ -1,1 +1,8 @@
-
+<<
+  \relative c' {
+    c4 d e f g a b c <c, e g>2 <c es g>
+  }
+  \addlyrics {
+    ド レ ミ ファ ソ ラ シ ド C Cm
+  }
+>>
